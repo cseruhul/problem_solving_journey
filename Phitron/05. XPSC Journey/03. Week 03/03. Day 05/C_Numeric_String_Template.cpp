720728f0c50f24f67e0094a744339ae3a7ab09a8
Ruhul Amin <ruhul.computer12@gmail.com>
2025-04-16 00:42:39 +0600

    Problem solving week 3 day 5

---
+++ Phitron/05. XPSC Journey/03. Week 03/03. Day 05/C_Numeric_String_Template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+class Ruhul_Amin
+{
+public:
+    void solve()
+    {
+        
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Ruhul_Amin ruhul;
+
+    ruhul.solve();
+
+    return 0;
+}
